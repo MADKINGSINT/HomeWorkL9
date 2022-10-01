9fd624f84f87de0d53176d4dcf429fc4491add7d
madkings <unconfigured@null.spigotmc.org>
2022-10-01 11:42:53 +0300

    programs writed

---
+++ Задание1/Program.cs
@@ -0,0 +1,19 @@
+﻿// : Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+System.Console.Write("введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+GetNumbersToN();
+
+
+void GetNumbersToN()
+{
+    int count = 1;
+    if (N >= count)
+    {
+        System.Console.WriteLine(N);
+        N = N - 1;
+        GetNumbersToN();
+    }
+}
---
+++ Задание2/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("введите число M:   ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("введите число N:   ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+void FindSum()
+{
+   if ( M > N)
+   {
+    for (; M >= N; N ++)
+    {
+        sum = sum + N;
+    }
+    }
+    else if (M < N)
+    {
+          for ( ; M <= N; M++ )
+          {
+            sum = sum + M;
+          }
+        
+
+    }
+
+    
+   }
+    
+
+FindSum();
+System.Console.WriteLine(sum);
